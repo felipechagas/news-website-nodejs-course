5596ab2d5e98345f8bb1d68dcfc6c71b0174c748
Felipe Chagas <felip.uece@gmail.com>
2019-01-17 20:19:00 +0000

    Lesson 29 Consign

--- app/routes/news.js
+++ app/routes/news.js
@@ -1,14 +1,10 @@
+var dbConnection = require('../../config/dbConnection')
+
 module.exports = function(app) {  
     
     app.get('/noticias', function(req, res) {
-        var mysql = require('mysql');
 
-        var connection = mysql.createConnection({
-            host : 'localhost',
-            user : 'root',
-            password : 'root',
-            database : 'news_portal'
-        });
+        var connection = dbConnection();        
 
         connection.query('select * from news;', function(error, result){
             res.render("ejs/noticias/noticias", {noticias : result}); 
