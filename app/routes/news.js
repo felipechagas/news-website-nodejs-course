43f81530737e114a197689048d603316ecd05994
Felipe Chagas <felip.uece@gmail.com>
2019-01-17 19:18:52 +0000

    Lesson 25 - Connecting to MySQL

--- app/routes/news.js
+++ app/routes/news.js
@@ -1,5 +1,17 @@
 module.exports = function(app) {  
+    
     app.get('/noticias', function(req, res) {
-        res.render("ejs/noticias/noticias");
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : 'root',
+            database : 'news_portal'
+        });
+
+        connection.query('select * from news;', function(error, result){
+            res.render("ejs/noticias/noticias", {noticias : result}); 
+        });
     });
 }
