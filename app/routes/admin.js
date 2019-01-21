44e28d46a9012782895c6874448f5349eb5c6384
Felipe Chagas <felip.uece@gmail.com>
2019-01-21 14:25:52 -0300

    Lesson 44 - Express validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,6 +6,22 @@ module.exports = function(app) {
     app.post('/news/save', function(req, res) {
         var news = req.body;
 
+        req.assert('headline','Titulo obrigatorio').notEmpty();
+        req.assert('resume','Resumo obrigatorio').notEmpty();
+        req.assert('resume','entre 10 e 100').len(10, 100);
+        req.assert('author','Autor obrigatorio').notEmpty();
+        // req.assert('dt_news','entre 10 e 100').notEmpty().isDate({format:'YYYY-MM-DD'});
+        req.assert('dt_news','entre 10 e 100').notEmpty();
+        req.assert('news','Noticia obrigatoria').notEmpty();
+ 
+        var erros = req.validationErrors();
+
+        if(erros) {
+            res.render("ejs/admin/form_add_noticia");
+
+            return;
+        }
+
         var connection = app.config.dbConnection();
         var newsModel = new app.models.NewsDAO(connection);
         
